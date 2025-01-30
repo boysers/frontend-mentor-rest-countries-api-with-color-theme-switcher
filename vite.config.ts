0b029a415452b5758c21243d1344945a5be1001c
boysers <50039110+boysers@users.noreply.github.com>
2025-01-30 12:30:23 +0100

    chore: added base url in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/frontend-mentor-rest-countries-api-with-color-theme-switcher",
   plugins: [react()],
-})
+});
